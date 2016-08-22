11acb12da2d106e0a1bf6658cd0654692b394cd0
willmedrano <willmedrano93@gmail.com>
2016-08-21 23:42:35 -0600

    version 5.15
    nada importante

--- resources/views/probandos.blade.php
+++ resources/views/probandos.blade.php
@@ -16,16 +16,22 @@
 
         {!! Form::open() !!}
     <div class="form-group">
+    <section>
+        
+    
+        
+    
     	{!! Form::label('Nombre: ') !!}
-    	{!! Form::text('name',null,['class'=>'form-control', 'placeholder'=>'Ingese el nombre del proveedor', requiered]) !!}
+    	{!! Form::text('name',null,['class'=>'form-control', 'placeholder'=>'Ingese el nombre del proveedor', 'requiered', 'size'=> 10, 'maxlength'=>4]) !!}
 
         {!! Form::label('Apellido: ') !!}
         {!! Form::text('name',null,['class'=>'form-control', 'placeholder'=>'Ingese el Apellido del proveedor']) !!}
+     </section>
     </div>
 {!! Form::close() !!}
 
         
-        <!--<p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>-->
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
       </div>
 </body>
 </html>
