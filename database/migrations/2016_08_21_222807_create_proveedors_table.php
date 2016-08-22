6825812a28be2dfd9cf079d4bf2846542b359b63
willmedrano <willmedrano93@gmail.com>
2016-08-21 22:42:06 -0600

    vesion 5.13
    correcion tabla proveedor

--- database/migrations/2016_08_21_222807_create_proveedors_table.php
+++ database/migrations/2016_08_21_222807_create_proveedors_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProveedoreTable extends Migration
+class CreateProveedorsTable extends Migration
 {
     /**
      * Run the migrations.
