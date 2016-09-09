8a8647ade95c487a52f49fcde03ebffc0b263bbe
willmedrano <willmedrano93@gmail.com>
2016-09-09 16:18:19 -0600

    version 6.1.2
    maldito jonthan

--- resources/views/inventario/index.blade.php
+++ resources/views/inventario/index.blade.php
@@ -1,5 +1,96 @@
- @extends('probandos')
+@extends('probandos')
 
 @section('content')
-jsbhsh
+<style type="text/css" >
+    
+
+
+.bigicon {
+    font-size: 35px;
+    color: #36A0FF;
+}
+legend{
+    color: #36A0FF;
+}
+}
+    
+</style>
+
+<div class="sidebar-overlay" id="sidebar-overlay"></div>
+
+                <article class="content forms-page">
+                    <div class="title-block">
+                        <h3 class="title">
+        Inventario
+    </h3>
+                        <p class="title-description"> Registro de Producto </p>
+                    </div>
+            <!--<div class="subtitle-block">
+                        <h3 class="subtitle">
+        Formulario de Producto
+    </h3> </div>-->
+                    <section class="section">
+                        <div class="row sameheight-container">
+                            <div >
+                                <div class="card card-block sameheight-item" >
+                                    <div class="title-block" align="center">
+                                        <h3 class="title">
+                        Formulario de producto
+                    </h3> </div>
+
+
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-center" align="center">Producto</legend>
+
+                        <div class="form-group">
+
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-book bigicon icon_nav" ></i></span>
+                            <div class="col-md-8">
+
+                                <input id="fname" name="name" type="text" placeholder="Codigo del Producto" class="form-control">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-adjust bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="lname" name="name" type="text" placeholder="Apellido" class="form-control">
+                            </div>
+                        </div>
+<br>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Correo " class="form-control">
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-xs-3">
+                                <input id="phone" name="phone" type="text" placeholder="Telefono" class="form-control">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="message" placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7"></textarea>
+                            </div>
+                        </div>
+                            <br>
+                        <div class="form-group">
+                            <div class="col-md-12 text-center" align="center">
+                                <button type="submit"  class="btn btn-primary btn-lg">Guardar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+  
 @stop
